9555331cce1497ca6d44d14419e0567f3955829c
Samuel Zanatta <126922020+Zanatta97@users.noreply.github.com>
2026-03-23 10:14:03 -0300

    Alterado serviço de logging para gravar dados no arquivo de forma assincrona para evitar erros de concorrencia

--- ApiMetasAnalistas/Logging/CustomLogger.cs
+++ ApiMetasAnalistas/Logging/CustomLogger.cs
@@ -5,6 +5,7 @@ namespace ApiMetasAnalistas.Logging
     {
         readonly string _name;
         readonly CustomLoggerProviderConfiguration _config;
+        private static readonly object _lockObj = new object();
 
         public CustomLogger(string name, CustomLoggerProviderConfiguration config)
         {
@@ -22,7 +23,7 @@ namespace ApiMetasAnalistas.Logging
             return logLevel >= _config.LogLevel;
         }
 
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
             Func<TState, Exception?, string> formatter)
         {
             if (!IsEnabled(logLevel))
@@ -31,19 +32,21 @@ namespace ApiMetasAnalistas.Logging
             }
 
             string message = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {logLevel} - {eventId.Id} - {_name}: {formatter(state, exception)}";
-            WriteMessageInFile(message);
+            await WriteMessageInFile(message);
         }
 
-        private void WriteMessageInFile(string message)
+        private async Task WriteMessageInFile(string message)
         {
             string logFilePath = _config.LogPath + _config.LogFile;
 
-            using (StreamWriter streamWriter = new StreamWriter(logFilePath, true))
+            //Pode afetar o desempenho em cenários de alta concorrência, mas garante a integridade dos dados no arquivo de log.
+            lock (_lockObj) // Garante que somente 1 thread escreva no arquivo de log por vez
             {
                 try
                 {
-                    streamWriter.WriteLine(message);
-                    streamWriter.Close();
+                    using var stream = new FileStream(logFilePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                    using var writer = new StreamWriter(stream);
+                    writer.WriteLine(message);
                 }
                 catch (Exception)
                 {
--- ApiMetasAnalistas/obj/Debug/net10.0/ApiMetasAnalistas.AssemblyInfo.cs
+++ ApiMetasAnalistas/obj/Debug/net10.0/ApiMetasAnalistas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adb98087b9695771fa332aa7bf0abc31436b351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5064bfb476d8b9ac2215f5b817cff4b4ac6704")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
