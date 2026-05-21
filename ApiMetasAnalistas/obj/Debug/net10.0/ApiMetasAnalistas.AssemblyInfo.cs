199ee07b02d1ab8c7aad533b35cb87746897b5e8
Samuel Zanatta <126922020+Zanatta97@users.noreply.github.com>
2026-05-21 17:39:48 -0300

    Só pra sair o erro

--- ApiMetasAnalistas/obj/Debug/net10.0/ApiMetasAnalistas.AssemblyInfo.cs
+++ ApiMetasAnalistas/obj/Debug/net10.0/ApiMetasAnalistas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e25eb3b60998f28af4cbf6020f3e3e6d0dee09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbf9a2dcc89d40b9a34ad230ef21b15c4104d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiMetasAnalistas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
